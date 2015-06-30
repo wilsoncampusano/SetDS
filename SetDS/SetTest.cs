341524921fee7daa4b0de8506705e02807046a9d
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2015-06-30 08:46:25 -0400

    two test added: can add and can know if set is empty

--- SetDS/SetTest.cs
+++ SetDS/SetTest.cs
@@ -14,5 +14,27 @@ namespace SetDS
             Set set = new Set(capacity);
             Assert.AreEqual(capacity, set.Capacity);
         }
+
+        [TestMethod]
+        public void ICanAddAnElement_ThenICanGetItBack()
+        {
+            var four = 4;
+            var set = new Set(1);
+            set.Add(four);
+            Assert.AreEqual(four, set.Get());
+        }
+
+        [TestMethod]
+        public void IfIAddOneElement_ThenItShouldBeNotEmpty()
+        {
+            var four = 4;
+            var set = new Set(1);
+
+            Assert.IsTrue(set.IsEmpty());
+            set.Add(four);
+            Assert.IsFalse(set.IsEmpty());
+        }
+
+
     }
 }
--- SetDS_Implementation/Set.cs
+++ SetDS_Implementation/Set.cs
@@ -8,10 +8,28 @@ namespace SetDS_Implementation
     public class Set
     {
         public int Capacity { get; private set; }
+        private int element;
+        private int _elementAdded=0;
 
         public Set(int capacity)
         {
             Capacity = capacity;
+        }
+
+        public void Add(int value)
+        {
+            element = value;
+            _elementAdded++;
+        }
+
+        public int Get()
+        {
+            return element;
+        }
+
+        public bool IsEmpty()
+        {
+            return !(Capacity == _elementAdded);
         }
     }
 }
