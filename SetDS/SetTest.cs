ec1cd01a40f39000a08d2c1b2441abfc057dcb06
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2015-06-30 08:22:16 -0400

    set implementation library

---
+++ SetDS/SetTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SetDS_Implementation;
+
+namespace SetDS
+{
+    [TestClass]
+    public class WithASet
+    {
+        [TestMethod]
+        public void ICanCreateOneWithACapacity()
+        {
+            var capacity = 10;
+            Set set = new Set(capacity);
+            Assert.AreEqual(capacity, set.Capacity);
+        }
+    }
+}
---
+++ SetDS_Implementation/Set.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetDS_Implementation
+{
+    public class Set
+    {
+        public int Capacity { get; private set; }
+
+        public Set(int capacity)
+        {
+            Capacity = capacity;
+        }
+    }
+}
