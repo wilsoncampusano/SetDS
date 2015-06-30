6a33f552c4523957c221b65f423130c27ed148c6
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2015-06-30 08:04:13 -0400

    specs environment ready

---
+++ SetDS/Environment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SetDS
+{
+    [TestClass]
+    public class Environment
+    {
+        [TestMethod]
+        public void EnvironmentFails()
+        {
+            Assert.Fail();
+        }
+    }
+}
